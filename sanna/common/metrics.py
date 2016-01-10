f14b36d3920e6ab5e41b203c950fe53b2de7e901
Milton Bose <milton.bose@gmail.com>
2016-01-09 21:46:33 -0800

    Kappa loss

--- sanna/common/metrics.py
+++ sanna/common/metrics.py
@@ -6,6 +6,35 @@ from theano import tensor as T
 
 from .functions import *
 
+def mean_kappa_loss(Y, y, y_distro=None, power=2, class_weights=None,
+                    n_classes=None):
+
+    if y_distro is None:
+        y_distro = np.ones(n_classes) / n_classes
+
+    actual_distro = T.constant(
+            np.asarray(y_distro)[:, None]
+            )
+    ycols = Y.shape[1]
+    a = T.arange(ycols)
+    w_num = a.dimshuffle('x', 0) - y .dimshuffle(0, 'x')
+    w = a.dimshuffle(0, 'x') - a.dimshuffle('x', 0)
+    if power == 1:
+        w = abs(w)
+        w_num = abs(w_num)
+    else:
+        w = w ** power
+        w_num = w_num ** power
+
+    num = T.sum(Y * w_num, axis = -1)
+    den = T.dot(T.dot(Y, w), actual_distro).ravel()
+
+    m = T.sum(num / den)
+    if class_weights is None:
+        return T.mean(m)
+    else:
+        return T.mean(class_weights[y] * m)
+
 
 def mean_weighted_loss(Y, y, power=1, class_weights=None):
 
